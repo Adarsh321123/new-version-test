44f41b382c6e4c40dd74e35dcf1a7982600f1450
Adarsh321123 <adarshk1234567@gmail.com>
2024-05-25 23:56:37 -0700

    [LeanCopilotBot] `sorry` Removed by Lean Copilot

--- NewVersionTest/Logic.lean
+++ NewVersionTest/Logic.lean
@@ -3,7 +3,7 @@ open Real
 open Function
 
 theorem example_one_conjunction (p q r s : Prop) (h : p → r) (h' : q → s) : p ∧ q → r ∧ s := by
-  sorry
+  tauto
 
 theorem example_seven_existential (a b c : ℤ) (h₁ : a ∣ b) (h₂ : b ∣ c) : a ∣ c := by
-  sorry
+  exact h₁.trans h₂
