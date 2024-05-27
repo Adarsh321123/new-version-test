3508f1f7d21f7c31ec7f472d0f5af026971661b8
Adarsh321123 <adarshk1234567@gmail.com>
2024-05-27 13:53:03 -0700

    remove files

--- NewVersionTest.lean
+++ NewVersionTest.lean
@@ -1,5 +1,3 @@
 -- This module serves as the root of the `NewVersionTest` library.
 -- Import modules here that should be built as part of the library.
 import «NewVersionTest».Basic
-import «NewVersionTest».Logic
-import «NewVersionTest».Complex
--- NewVersionTest/Complex.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib
-open Real
-
-theorem abs_val (a b : ℝ) : |a * b| ≤ (a ^ 2 + b ^ 2) / 2 := by
-  sorry
