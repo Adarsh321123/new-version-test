6eb56a01c8febf938bcc166cd64339e223a99086
Adarsh321123 <adarshk1234567@gmail.com>
2024-06-01 13:50:53 -0700

    remvoe hard

--- NewVersionTest/Basic.lean
+++ NewVersionTest/Basic.lean
@@ -1,12 +1,7 @@
-import Mathlib
 open Nat (add_assoc add_comm)
-open Real
 
 theorem foo (a : Nat) : a + 1 = Nat.succ a := by
   sorry
 
 theorem bar (a b : Nat) : a + b = b + a := by
   rw[add_comm]
-
-theorem abs_val (a b : ℝ) : |a * b| ≤ (a ^ 2 + b ^ 2) / 2 := by
-  sorry
