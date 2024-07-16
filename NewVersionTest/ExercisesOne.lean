9ec5cbea84b884e8f6f79b464005de872a32b62e
Adarsh321123 <adarshk1234567@gmail.com>
2024-07-16 13:02:09 -0700

    remove proof and add sorry

--- NewVersionTest/ExercisesOne.lean
+++ NewVersionTest/ExercisesOne.lean
@@ -10,5 +10,6 @@ theorem example_three_rw (a b c d e : ℝ) (h : a = b + c) (h' : b = d - e) : a 
 theorem example_five_rw (a b c : ℝ) : exp (a + b + c) = exp a * exp b * exp c := by
   -- rw [exp_add (a + b) c]  -- exp_add lemma says exp (x + y) = exp x * exp y
   -- rw [exp_add a b]
-  rw [exp_add, exp_add]  -- Lean rewrites the first matching expression, so we don't
+  -- rw [exp_add, exp_add]  -- Lean rewrites the first matching expression, so we don't
   -- need to provide arguments
+  sorry
